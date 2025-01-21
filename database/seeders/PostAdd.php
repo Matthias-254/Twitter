be8f42fbe76e0b37ad00aaef4dfdc37c4ce0204c
Matthias-254 <lannoo.matthias@hotmail.com>
2025-01-21 15:08:01 +0100

    layout updated

--- database/seeders/PostAdd.php
+++ database/seeders/PostAdd.php
@@ -19,5 +19,19 @@ class PostAdd extends Seeder
             'image_path' => 'https://www.marketingfacts.nl/wp-content/uploads/images/imagesblog/f1b5lfuzmwvzs7nslwbn.jpg',
             'user_id' => 1,
         ]);
+
+        Post::create([
+            'title' => 'Twitter is the best!',
+            'content' => 'Twitter is the best social media platform! #twitter',
+            'image_path' => 'https://cdn.pixabay.com/photo/2016/11/01/12/09/twitter-logo-1788039_1280.jpg',
+            'user_id' => 2,
+        ]);
+
+        Post::create([
+            'title' => 'BetaStar!',
+            'content' => 'BetaStar will be the best company in the world!',
+            'image_path' => 'https://plutuseducation.com/blog/wp-content/uploads/2024/12/image-39-e1734437710270-768x461.jpeg',
+            'user_id' => 2,
+        ]);
     }
 }
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -6,18 +6,16 @@
             padding: 2px 5px;
             float: right;
         }
-
         #post-edit{
             border-radius: 5px;
             background-color: rgb(0, 128, 255);
             padding: 2px 5px;
         }
-
         #post-del:hover{
             cursor: pointer;
         }
         img{
-            width: 50%;
+            width: 90%;
             height: auto;
             margin: 0 auto;
         }
@@ -25,18 +23,30 @@
             margin-top: 20px;
             font-size: 1.8rem;
             font-weight: bold;
-            text-align: center;
+            text-align: left;
             margin-bottom: 10px;
+            margin-left: 5%;
+            margin-right: 5%;
         }
         #content{
             font-size: 1.1rem;
-            margin-bottom: 10px;
-            text-align: center;
+            text-align: left;
+            margin-bottom: 5vh;
+            margin-left: 5%;
+            margin-right: 5%;
         }
         #date{
             font-size: 0.9rem;
             margin-bottom: 10px;
             color: gray;
+        }
+        #post{
+            margin-top: 5vh;
+        }
+        #container{
+            width: 45%;
+            margin: 0 auto;
+            padding-bottom: 10vh;
         }
     </style>
 
@@ -46,18 +56,19 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    @foreach ($posts as $post)
-                        <p>{{$post->poster->name}} • {{$post->created_at->format('H\ui -\ d/m/y')}}</p>
+    <div id="container">
+        @foreach ($posts as $post)
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8" id="post">
+                <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 text-gray-900 dark:text-gray-100">
+                        <p><b>{{$post->poster->name}}</b> • <span id="date">{{$post->created_at->format('H\ui -\ d/m/y')}}</span></p>
                         <h3>{{$post->title}}</h3>
-                        <p id="content">{{$post->content}}</p>
+                        <p id="content">{{$post->content}}</p>    
                         <img src="{{ URL($post->image_path) }}" alt="News logo">
-                    @endforeach
+                    </div>
                 </div>
             </div>
-        </div>
+        @endforeach
     </div>
+                
 </x-app-layout>
